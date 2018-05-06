45d531c7e24b03b8dfe255b11c9a7f3de48528bf
Agent Fitz ;-) <fitz.bao@outlook.com>
2018-05-06 17:24:02 +0800

    fix bug

--- Steam-Origin-REG-EI.bat
+++ Steam-Origin-REG-EI.bat
@@ -2,12 +2,12 @@
 title Steam/Origin REG EI By F1tz 
 SET now=%date:~0,4%%date:~5,2%%date:~8,2%
 mkdir %now%
-echo ���ڵ���Steamע���
+echo 正在导出Steam注册表
 REG EXPORT HKEY_CLASSES_ROOT\steam %now%\SteamURL.reg
 REG EXPORT HKEY_CURRENT_USER\Software\Classes\steam %now%\SteamURL_HCU.reg
 REG EXPORT HKEY_CURRENT_USER\Software\Valve %now%\Valve_HCUS.reg
 REG EXPORT HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve %now%\Valve_HLMSW64.reg
-echo ���ڵ���Originע���
+echo 正在导出Origin注册表
 REG EXPORT "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Electronic Arts" %now%\EA.reg
 REG EXPORT "HKEY_LOCAL_MACHINE\SOFTWARE\EA Games" %now%\EAGAMES.reg
 REG EXPORT "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\EA Games" %now%\EAGAMES64.reg
@@ -15,11 +15,11 @@ REG EXPORT "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Origin" %now%\Origin.reg
 REG EXPORT "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Origin Games" %now%\OriginGames.reg
 REG EXPORT "HKEY_CURRENT_USER\Software\Electronic Arts" %now%\EA_HCU.reg
 echo @echo off>%now%\REG_IMPORTER.bat
-echo for %%a in (%%~dp0*.reg) do REG IMPORT %%a>>%now%\REG_IMPORTER.bat
-echo echo ע����Ѿ��������>>%now%\REG_IMPORTER.bat
+echo for %%%%a in (%%~dp0*.reg) do REG IMPORT %%%%a>>%now%\REG_IMPORTER.bat
+echo echo 注册表已经导入完成>>%now%\REG_IMPORTER.bat
 echo pause>>%now%\REG_IMPORTER.bat
-echo ����ע�����Ŀ�ѵ�����ɷ��ڡ�%~dp0%now%��Ŀ¼��
-echo �Ժ��ʹ�á�REG_IMPORTER.bat�������µ���ע�����Ŀ
+echo 所有注册表项目已导出完成放在“%~dp0%now%”目录下
+echo 稍后可使用“REG_IMPORTER.bat”，重新导入注册表项目
 echo.
-echo �������⣬���� https://github.com/f1tz/Steam-Origin-REG-EI ��ϵ����
+echo 如有问题，请在 https://github.com/f1tz/Steam-Origin-REG-EI 联系到我
 pause
